a3cddc5aaf455e3de7a7d1621914a3012cc4fa8f
callyral <125226346+callyral@users.noreply.github.com>
2023-10-27 19:56:46 -0300

    river: Fix typos in scripts/autostart.sh

--- home/dot_config/river/scripts/executable_autostart.sh
+++ home/dot_config/river/scripts/executable_autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-pgrep pipewire || pipewire &
-pgrep pipewire-pulese || pipewire-pulse &
+pgrep -x pipewire || pipewire &
+pgrep -x pipewire-pulse || pipewire-pulse &
 pgrep wireplumber || wireplumber &
 
 waybar &
 
-pgreo dunst || dunst &
+pgrep dunst || dunst &
 
 pgrep nm-applet || nm-applet &
 pgrep copyq || copyq &
@@ -14,7 +14,7 @@ pgrep gammastep || gammastep-indicator -m wayland &
 gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-purple-dark" &
 gsettings set org.gnome.desktop.interface cursor-theme "Catppuccin-Macchiato-Peach-Cursors" &
 
-pgreap swayidle || swayidle -w \
+pgrep swayidle || swayidle -w \
          timeout 300 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
          timeout 600 'swaylock -f -c 000000' \
          before-sleep 'swaylock -f -c 000000' &
